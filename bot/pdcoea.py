572f32d52bd7a47f28467f077fdc1627d97a6fd0
matthewh <115488877+matthewh26@users.noreply.github.com>
2024-07-22 15:22:42 +0100

    bot changes

--- bot/pdcoea.py
+++ bot/pdcoea.py
@@ -1,4 +1,4 @@
-import pyspiel
+import torch
 import numpy as np
 from quoridor_agents import NeuralNetwork
 
@@ -6,13 +6,15 @@ from quoridor_agents import NeuralNetwork
 class PdcoeaBot():
     def __init__(self,state_dict):
         self.state_dict = state_dict
-        self.network = NeuralNetwork()
-        self.network.load_state_dict(self.state_dict)
+        self.network = NeuralNetwork(np.zeros(222369))
+        self.network.load_state_dict(torch.load(self.state_dict))
 
     def step(self,state):
 
         legal_actions = state.legal_actions(state.current_player())
-        out = self.network(state.observation_tensor())
+        input = torch.Tensor(state.observation_tensor())
+        out = self.network(input)
+        out = out.detach().numpy()
         while True:
             action = np.argmax(out)
             if action in legal_actions:
@@ -20,5 +22,6 @@ class PdcoeaBot():
             else:
                 out[action] = np.min(out)
         return action 
+
 
 
